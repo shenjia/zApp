730a3e2d39837eb6924af64638f2c5d796dd5cf3
shenjia <zshenjia@gmail.com>
2013-11-26 13:13:55 +0800

    split resource configs

--- code/common/config/define.php
+++ code/common/config/define.php
@@ -7,5 +7,4 @@ if (!defined('PAGECOUNT')) define('PAGECOUNT', 15);
  * 环境配置
  */
 require_once __DIR__ . '/' . ENV . '/debug.php';
-require_once __DIR__ . '/' . ENV . '/resource.php';
 require_once __DIR__ . '/' . ENV . '/database.php';
