8b7eb55066c9243e8604e2af279e4f37247bb577
shenjia <zshenjia@gmail.com>
2013-11-26 08:13:12 +0800

    refactor

--- code/protected/commands/TestCommand.php
+++
@@ -1,8 +0,0 @@
-<?php
-class TestCommand extends CConsoleCommand
-{
-    public function actionIndex($args) 
-    {
-        echo 'test' . PHP_EOL;
-    }
-}
--- code/protected/views/layouts/main.php
+++
@@ -1,5 +0,0 @@
-<?php $this->beginContent('//layouts/headfoot'); ?>
-<div id="page" class="main">
-    <?php echo $content; ?>
-</div>
-<?php $this->endContent(); ?>
